01adbc9f71d0b3f2aaed376612eecfc39173ad05
Kojo Acquah <kojo.acquah@hotmail.com>
2017-10-13 19:03:21 -0300

    Submission 05 - Web Tech

---
+++ CSCI 3431 Operating Systems/Assignment 2/SudokuTest/src/sudokutest/SudokuTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokutest;
+
+/**
+ *
+ * @author kojo
+ */
+public class SudokuTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//get file name
+//open file 
+//read lines into arrays
+    }
+    
+}
