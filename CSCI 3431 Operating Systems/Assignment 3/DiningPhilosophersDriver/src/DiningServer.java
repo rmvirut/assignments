9a41a76a191d94b845f218f79ad0c855d38e897f
Kojo Acquah <kojo.acquah@hotmail.com>
2017-11-02 13:26:45 -0300

    Assignment 3 - OS
    Started assignement 3 on Operating Systems

---
+++ CSCI 3431 Operating Systems/Assignment 3/DiningPhilosophersDriver/src/DiningServer.java
@@ -0,0 +1,15 @@
+
+/**
+ * DiningServer.java
+ * This class contains the methods called by the  philosophers. 
+ **/
+
+public interface DiningServer 
+{  
+   // called by a philosopher when he/she wants to eat 
+   public void takeForks(int pNumber);
+  
+   // called by a philosopher when he/she are done eating 
+   public void returnForks(int pNumber);
+   
+}
---
+++ CSCI 3431 Operating Systems/Assignment 3/DiningPhilosophersDriver/src/DiningServerImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kojo
+ */
+public class DiningServerImpl implements DiningServer {
+
+    @Override
+    public void takeForks(int pNumber) {
+
+    }
+
+    @Override
+    public void returnForks(int pNumber) {
+
+    }
+}
