da65d9ce9bc7e404d5e300458c10a3e5c0c7f3e1
Kojo Acquah <kojo.acquah@hotmail.com>
2017-11-21 21:15:12 -0400

    Update time.php

--- CSCI 2355 Internet Technologies and Web programming/submissions/submission11/time.php
+++ CSCI 2355 Internet Technologies and Web programming/submissions/submission11/time.php
@@ -1,5 +1,7 @@
+<?php
+//format for "Its [day], [month] [dayOfMonth] Our time is [hour]:[minute] [meridian]"
 $format = '\I\t\'\s l, F jS \n \O\u\r \t\i\m\e \i\s g:i a';
 
 $x = date($format);
 
-echo $x;
+echo $x;//return the results
