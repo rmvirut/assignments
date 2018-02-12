d92ed5cf7f735fb7fa0de3d12cbdbbdce8a918e6
Kojo Acquah <kojo.acquah@hotmail.com>
2018-02-12 04:34:32 -0400

    Assignemnt 1 for Graphics

---
+++ CSCI 4471 - Graphics/Assignment 1/2D Screen Saver/2D Screen Saver/2DScreenSaver.c
@@ -0,0 +1,19 @@
+#include <GL/freeglut.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+
+void init(void)
+{
+	
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main()
+{
+	init();
+	return 0;
+}
