04afab519c95887d52146f66025412bdef2c90c9
Kojo <kojoacquah7@gmail.com>
2019-02-15 17:50:58 -0400

    System Security assing 4

---
+++ playground/test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class test {
+
+    public static void main(String [] args){
+        
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+
+    }
+
+}
